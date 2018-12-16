7d56adcde4fd2d1b95c9a57ee87c9055ce1f4657
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2018-12-16 20:21:43 +0100

    entrypoint

--- app/docker/php/docker-entrypoint.sh
+++ app/docker/php/docker-entrypoint.sh
@@ -11,9 +11,9 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
 	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
 	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
 
-	#if [ "$APP_ENV" != 'prod' ]; then
-	#	composer install --prefer-dist --no-progress --no-suggest --no-interaction
-	#fi
+	if [ "$APP_ENV" != 'prod' ]; then
+		composer install --prefer-dist --no-progress --no-suggest --no-interaction
+	fi
 fi
 
 exec docker-php-entrypoint "$@"
